68c56d2e59371da29c1c510f8a8b9a15878bc830
Aidan-The-Dev <aidanthedev@zohomail.com>
2025-02-10 04:48:08 -0600

    Complete overhaul of the website

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    const birthDate = new Date('2005-06-07');
+    const currentDate = new Date();
+
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const month = currentDate.getMonth();
+    const day = currentDate.getDate();
+
+    if (month < birthDate.getMonth() || (month === birthDate.getMonth() && day < birthDate.getDate())) {
+        age--;
+    }
+
+    document.getElementById('age').textContent = age;
+};
+
+document.getElementById('footer-year').textContent = new Date().getFullYear();
