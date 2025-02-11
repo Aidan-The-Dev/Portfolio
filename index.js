469a3fb7233f794ca098019f7c70f8814510533b
Aidan-The-Dev <aidanthedev@zohomail.com>
2025-02-11 02:32:04 -0600

    Fixed mobile UI

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ window.onload = function() {
 };
 
 document.getElementById('footer-year').textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+  
+    hamburger.addEventListener("click", function() {
+      navMenu.classList.toggle("active");
+    });
+  });  
